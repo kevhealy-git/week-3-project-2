de91879099737c7617425a1e020330a0aed025f5
Kevin Healy-Clarke <2405518+kevhealy-git@users.noreply.github.com>
2023-03-09 19:40:12 +0000

    Components added

---
+++ src/.d.ts
@@ -0,0 +1,1 @@
+declare module "week-3-design-systems";
--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Card } from "week-3-design-systems";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dashboard</h1>
+        <Card
+          alt="An image showing some mountains"
+          image="https://images.unsplash.com/photo-1674423094915-2546893e2636?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+          description="This card comes from week-3-design-systems package."
+        />
+        <Button label="This button comes from week-3-design-systems" />
       </header>
     </div>
   );
